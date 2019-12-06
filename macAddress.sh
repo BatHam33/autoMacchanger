cca6815520efbb30cdcc0b0d44fc2de1dc958909
BatHam33 <47986703+BatHam33@users.noreply.github.com>
2019-12-06 13:55:14 -0700

    Create macAddress.sh

---
+++ macAddress.sh
@@ -0,0 +1,46 @@
+
+#!/bin/bash
+ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+baseip=`echo $ip | cut -d"." -f1-3`
+baseip+=.0/24
+MACS=$(nmap -sP  "$baseip")
+declare -a arr
+while IFS= read -r line
+do
+if [[ ${line:0:1} == "M" ]]
+then
+    mac=$(echo $line | cut -d' ' -f 3 | cut -d':' -f 1-3)
+    arr+=("$mac")
+fi
+done <<< "$MACS"
+declare -a arrNums
+finalMac=00:00:00
+finalNum=0
+for i in "${!arr[@]}"
+do
+    finalMac="${arr[$i]}"
+    finalNum=0
+    for j in "${!arr[@]}"
+    do
+        if [[ "$finalMac" == "${arr[$j]}" ]]
+        then
+            finalNum+=1
+        fi
+    done
+    arrNums[$i]=$finalNum
+done
+finalMac=00:00:00
+finalNum=0
+for i in "${!arrNums[@]}"
+do
+    if [[ "${arrNums[$i]}" -gt "$finalNum" ]]
+    then
+        finalNum="${arrNums[$i]}"
+        finalMac="${arr[$i]}"
+        fi
+done
+finalMac+=:
+finalMac+=$(./random.sh)
+ifconfig eth0 down
+ifconfig eth0 hw ether $finalMac
+ifconfig eth0 up
